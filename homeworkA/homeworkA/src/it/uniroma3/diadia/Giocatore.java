2d1e413bdd65ba4abcbc37b52f8bbce32259449b
Lindascrub <165819955+Lindascrub@users.noreply.github.com>
2025-03-26 13:01:13 +0100

    prima

---
+++ homeworkA/homeworkA/src/it/uniroma3/diadia/Giocatore.java
@@ -0,0 +1,36 @@
+package it.uniroma3.diadia;
+
+public class Giocatore {
+	private int cfu;
+	private Borsa borsa;
+	
+
+	public Giocatore() {
+		this.setBorsa(new Borsa());
+	}
+
+	public int getCfu() {
+		return cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+
+	public Borsa getBorsa() {
+		return borsa;
+	}
+
+	public void setBorsa(Borsa borsa) {
+		this.borsa = borsa;
+	}
+
+    public boolean prendereAttrezzo(Attrezzo nomeAttrezzo) {
+        return this.borsa.addAttrezzo(nomeAttrezzo);
+    }
+
+    public Attrezzo[] posareAttrezzo(String nomeAttrezzo) {
+        return this.borsa.removeAttrezzo(nomeAttrezzo);
+    }
+
+}
