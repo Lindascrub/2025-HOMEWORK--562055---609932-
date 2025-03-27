5686dec9e9ae9bbd28336eb4bf2919b263b53c8c
Lindascrub <165819955+Lindascrub@users.noreply.github.com>
2025-03-27 09:55:14 +0100

    prima

--- homeworkA/test/it/uniroma3/testBorsa.java
+++
@@ -1,5 +0,0 @@
-package it.uniroma3;
-
-public class testBorsa {
-
-}
--- homeworkA/test/it/uniroma3/testLabirinto.java
+++
@@ -1,8 +0,0 @@
-package it.uniroma3;
-
-import org.junit.jupiter.api.BeforeEach;
-
-public class testLabirinto {
-	@BeforeEach 
-	test ogni stanza del labirinto
-}
