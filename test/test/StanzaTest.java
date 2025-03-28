cfd97757711474b20bc3667b2141bc5959bebacd
Lindascrub <165819955+Lindascrub@users.noreply.github.com>
2025-03-28 15:57:24 +0100

    test

---
+++ test/test/StanzaTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StanzaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testHasAttrezzo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAttrezzo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAttrezzo() {
+		fail("Not yet implemented");
+	}
+
+}
