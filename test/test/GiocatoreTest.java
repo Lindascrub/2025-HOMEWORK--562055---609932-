c7cec55cabbaa8a5f931a9738511db95b2507042
Lindascrub <165819955+Lindascrub@users.noreply.github.com>
2025-04-02 19:59:45 +0200

    MARINELLA

--- test/test/GiocatoreTest.java
+++ test/test/GiocatoreTest.java
@@ -1,19 +1,42 @@
 package test;
 
+
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.giocatore.Giocatore;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GiocatoreTest {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    
+    private Giocatore giocatore;
+    private Attrezzo attrezzo;
+    
+    @BeforeEach
+    void setUp() {
+        giocatore = new Giocatore();
+        attrezzo = new Attrezzo("Spada", 3);
+    }
+    
+    @Test
+    void testCfuIniziali() {
+        assertEquals(3, giocatore.getCfu());
+    }
+ 
+    
+    @Test
+    void testPrendereAttrezzo() {
+        assertTrue(giocatore.prendereAttrezzo(attrezzo));
+        assertNotNull(giocatore.getBorsa().getAttrezzo("Spada"));
+    }
+    
+    @Test
+    void testPosareAttrezzo() {
+        giocatore.prendereAttrezzo(attrezzo);
+        giocatore.posareAttrezzo("Spada");
+        assertNull(giocatore.getBorsa().getAttrezzo("Spada"));
+    }
 }
