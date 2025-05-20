5e02b010a3f15fc1863fbd47c3749f2740fa1e4b
Margherita <mfidenzi14@gmail.com>
2025-05-20 18:14:58 +0200

    finire di implementare IOSimulator e relativi test

--- src/it/uniroma3/diadia/IOSimulator.java
+++ src/it/uniroma3/diadia/IOSimulator.java
@@ -2,6 +2,15 @@ package it.uniroma3.diadia;
 
 public class IOSimulator implements IO {
 
+	private String[] inputDaLeggere;
+	private String[] messaggiStampati;
+	private int contatore;
+	
+	public IOSimulator(String[] input) {
+		this.inputDaLeggere=input;
+		this.messaggiStampati=new String[6];
+	}
+	
 	@Override
 	public void mostraMessaggio(String messaggio) {
 		// TODO Auto-generated method stub
