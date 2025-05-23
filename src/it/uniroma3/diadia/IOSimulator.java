e0caeaadfef033e00aa0819b9768dfa7018e5a22
Margherita <mfidenzi14@gmail.com>
2025-05-23 13:36:37 +0200

    implementati tutti i metodi di IOSimulator

--- src/it/uniroma3/diadia/IOSimulator.java
+++ src/it/uniroma3/diadia/IOSimulator.java
@@ -7,23 +7,83 @@ public class IOSimulator implements IO {
 	private int comandoCorrente;
 	private int messaggioCorrente;
 	
-	public IOSimulator(String[] inputComandi) {
+	public IOSimulator(String[] inputComandi, String[] output) {
+		this.messaggi=output; 
 		this.comandi=inputComandi;
 		this.comandoCorrente=0;
+		this.messaggioCorrente=0;
 	}
 	
+	
+	
+	public String[] getComandi() {
+		return comandi;
+	}
+
+
+
+	public void setComandi(String[] comandi) {
+		this.comandi = comandi;
+	}
+
+
+
+	public String[] getMessaggi() {
+		return messaggi;
+	}
+
+
+
+	public void setMessaggi(String[] messaggi) {
+		this.messaggi = messaggi;
+	}
+
+
+
+	public int getComandoCorrente() {
+		return comandoCorrente;
+	}
+
+
+
+	public void setComandoCorrente(int comandoCorrente) {
+		this.comandoCorrente = comandoCorrente;
+	}
+
+
+
+	public int getMessaggioCorrente() {
+		return messaggioCorrente;
+	}
+
+
+
+	public void setMessaggioCorrente(int messaggioCorrente) {
+		this.messaggioCorrente = messaggioCorrente;
+	}
+
+
+
 	@Override
 	public void mostraMessaggio(String messaggio) {
 		// TODO Auto-generated method stub
+		if(messaggioCorrente<messaggi.length) {
+			messaggi[messaggioCorrente]=messaggio;
+			messaggioCorrente++;
+		}
+		
 		
 	}
 
 	@Override
 	public String leggiRiga() {
 		// TODO Auto-generated method stub
-		String riga=comandi[comandoCorrente];
-		comandoCorrente++;
-		return riga;
+		if(comandoCorrente<comandi.length) {
+			String riga=comandi[comandoCorrente];
+			comandoCorrente++;
+			return riga;
+		}
+		return "fine";
 	}
 
 }
