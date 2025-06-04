3a02b44352723c59dbe93298d0422e892246366e
Margherita <mfidenzi14@gmail.com>
2025-06-04 19:59:12 +0200

    introdotti Cane e Strega da implementare e testare

---
+++ src/it/uniroma3/diadia/personaggi/Cane.java
@@ -0,0 +1,27 @@
+package it.uniroma3.diadia.personaggi;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+public class Cane extends AbstractPersonaggio {
+
+	private Giocatore giocatore; 
+	private static final String MESSAGGIO_ABBAIO="Woof, woof!";
+	
+	public Cane(String nome, String presentaz) {
+		super(nome, presentaz);
+	}
+
+	@Override
+	public String agisci(Partita partita) {
+		String msg;
+		msg=MESSAGGIO_ABBAIO;
+		if(this.giocatore.getCfu()>0) {
+			this.giocatore.setCfu(0);
+		}
+		return msg;
+	}
+
+	
+	
+}
