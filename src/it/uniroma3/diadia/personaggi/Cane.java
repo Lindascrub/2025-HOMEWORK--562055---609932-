8661792846483f830ffe9a4c1e45eff76d29989b
Margherita <mfidenzi14@gmail.com>
2025-06-07 16:51:11 +0200

    finita classe Cane

--- src/it/uniroma3/diadia/personaggi/Cane.java
+++ src/it/uniroma3/diadia/personaggi/Cane.java
@@ -8,7 +8,7 @@ public class Cane extends AbstractPersonaggio {
 	/*NB: quando interagiamo con un cane, questi morde, togliendoci CFU a ogni morso*/
 	
 	private Giocatore giocatore; 
-	private static final String MESSAGGIO_ABBAIO="Woof, woof!";
+	private static final String MESSAGGIO_CANE="Woof, woof! Ti ho tolto un CFU";
 	
 	public Cane(String nome, String presentaz) {
 		super(nome, presentaz);
@@ -17,9 +17,9 @@ public class Cane extends AbstractPersonaggio {
 	@Override
 	public String agisci(Partita partita) {
 		String msg;
-		msg=MESSAGGIO_ABBAIO;
+		msg=MESSAGGIO_CANE;
 		if(this.giocatore.getCfu()>0) {
-			this.giocatore.setCfu(0);
+			this.giocatore.setCfu(this.giocatore.getCfu()-1);
 		}
 		return msg;
 	}
