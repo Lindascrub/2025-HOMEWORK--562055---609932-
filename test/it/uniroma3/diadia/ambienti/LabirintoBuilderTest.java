fdcdce1c4d93f713b48923e30c495683cd9efac7
Margherita <mfidenzi14@gmail.com>
2025-06-07 11:40:14 +0200

    finisci LabirintoBuilderTest

--- test/it/uniroma3/diadia/ambienti/LabirintoBuilderTest.java
+++ test/it/uniroma3/diadia/ambienti/LabirintoBuilderTest.java
@@ -2,12 +2,20 @@ package it.uniroma3.diadia.ambienti;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class LabirintoBuilderTest {
 
+	
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
 	@Test
-	void test() {
+	void testAddStanzaIniziale() {
 		fail("Not yet implemented");
 	}
 
